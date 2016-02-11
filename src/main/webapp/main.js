aef0add7fc01f46434bdb67ccb49e3077942aadb
linted <linted90@hotmail.com>
2016-02-11 15:14:35 -0500

    small changes

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,14 +1,11 @@
 function main() {
     var aptfinder_list = angular.module("aptfinder_list", []);
+    $scope.properties = ["1", "2", "3"];
     aptfinder_list.controller('populateList', function($scope) {
         //here we will query the DB and get the results for now here is some filler code
         $scope.properties = [
-            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
-            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
-            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
-            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
-            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
-        ]
+            "a", "b", "c"
+        ];
     })
 }
 
@@ -16,3 +13,9 @@ window.onload = function () {
     main();
     console.log("Done");
 }
+
+/*{HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"}, */
