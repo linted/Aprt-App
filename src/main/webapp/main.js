d35f3180143511575a09120ce5f932598902b121
linted <linted90@hotmail.com>
2016-02-11 13:33:47 -0500

    made some chagnes to a few things

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,8 +1,18 @@
 function main() {
-    listingHolder = document.getElementById("listingHolder");
-    
+    var aptfinder_list = angular.module("aptfinder_list", []);
+    aptfinder_list.controller('populateList', function($scope) {
+        //here we will query the DB and get the results for now here is some filler code
+        $scope.properties = [
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+            {HousingHeadline: "This is a house!", Location: "that one place", Price: "$9001", Lease: "yes?", thumb: "#"},
+        ]
+    })
 }
 
 window.onload = function () {
     main();
+    console.log("Done");
 }
