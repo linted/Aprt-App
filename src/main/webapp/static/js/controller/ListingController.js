307fb798c0994e6b461d2e6867c30a02b25f8bc4
linted <linted90@hotmail.com>
2016-03-06 20:28:46 -0500

    made a couple of changes

--- src/main/webapp/static/js/controller/ListingController.js
+++ src/main/webapp/static/js/controller/ListingController.js
@@ -1,86 +1,95 @@
 'use strict';
 
-App.controller('ListingController', ['$scope', 'ListingService', function($scope, ListingService) {
+App.controller('ListingController', ['$scope', 'ListingService', function ($scope, ListingService) {
     /******************* Variable declarations *******************/
-	var self = this;
-	//self.listing = {};   <- do we need this anymore?
-	self.listings = [];
+    var self = this;
+    //self.listing = {};   <- do we need this anymore?
+    self.listings = [];
     $scope.totalDisplayed = 20;
-	
+
     /******************* function declarations *******************/
-	self.fetchAllListings = function() {
-		ListingService.fetchAllListings()
-			.then(
-					function(d) {
-						console.log(d);
-						self.listings = d;
-					},
-					function(errResponse) {
-						console.error('Error while fetching listings');
-					}
-			);
-	};
-    
-    self.placeMarkers = function() {
-        for (var i =0 ; i < self.listings.length; i++ ) {
-            addMarker({lat:self.listings[i].lat, lng:self.listings[i].lng});
+    self.fetchAllListings = function () {
+        ListingService.fetchAllListings()
+            .then(
+                function (d) {
+                    console.log(d);
+                    self.listings = d;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching listings');
+                }
+            );
+    };
+
+    self.placeMarkers = function () {
+        for (var i = 0; i < self.listings.length; i++) {
+            addMarker({
+                lat: self.listings[i].lat,
+                lng: self.listings[i].lng
+            });
         }
     }
-	
-	$scope.loadMore = function () {
-		console.log("loading more");
-		$scope.totalDisplayed += 20;
-	};
-	
-	$scope.applyToSearch = function() {
-		var minPrice = document.getElementById('lowerPriceBound').value;
-		var maxPrice = document.getElementById('upperPriceBound').value;
-		
-		console.log("Min Price" + minPrice);
-		console.log("Max Price" + maxPrice);
-	}
-	
-	$scope.filterPrice = function(item) {
-//		console.log('filtering prices');
-		var minPrice = document.getElementById('lowerPriceBound').value;
-		var maxPrice = document.getElementById('upperPriceBound').value;
-		if (minPrice == '' && maxPrice == '') {
-//			console.log('prices were null');
-			return item.price;
-		} else if (minPrice == '' && maxPrice > 0) {
-			return item.price < maxPrice;
-		} else if (minPrice > 0 && maxPrice == '') {
-			return item.price > minPrice;
-		} else return ((item.price > minPrice && item.price < maxPrice));
-	}
-	
-	$scope.customOrder = function() {
-		var order = document.getElementById('SortBySelect').value;
-		console.log(order);
-	}
-    
-    $scope.MapOn = function() {
-        if($scope.mapCheckbox){
-        $scope.listCheckbox = false;}
-        else { $scope.listCheckbox = true;}
+
+    $scope.loadMore = function () {
+        console.log("loading more");
+        $scope.totalDisplayed += 20;
+    };
+
+    $scope.applyToSearch = function () {
+        var minPrice = document.getElementById('lowerPriceBound').value;
+        var maxPrice = document.getElementById('upperPriceBound').value;
+
+        console.log("Min Price" + minPrice);
+        console.log("Max Price" + maxPrice);
     }
-    
-    $scope.ListOn = function() {
-        if($scope.listCheckbox){
-        $scope.mapCheckbox = false;}
-        else { $scope.mapCheckbox = true;}
+
+    $scope.filterPrice = function (item) {
+        //		console.log('filtering prices');
+        var minPrice = document.getElementById('lowerPriceBound').value;
+        var maxPrice = document.getElementById('upperPriceBound').value;
+        if (minPrice == '' && maxPrice == '') {
+            //			console.log('prices were null');
+            return item.price;
+        } else if (minPrice == '' && maxPrice > 0) {
+            return item.price < maxPrice;
+        } else if (minPrice > 0 && maxPrice == '') {
+            return item.price > minPrice;
+        } else return ((item.price > minPrice && item.price < maxPrice));
     }
-    
+
+    $scope.customOrder = function () {
+        var order = document.getElementById('SortBySelect').value;
+        console.log(order);
+    }
+
+    $scope.MapOn = function () {
+        if ($scope.mapCheckbox) {
+            $scope.listCheckbox = false;
+        } else {
+            $scope.listCheckbox = true;
+        }
+    }
+
+    $scope.ListOn = function () {
+        if ($scope.listCheckbox) {
+            $scope.mapCheckbox = false;
+        } else {
+            $scope.mapCheckbox = true;
+        }
+    }
+
     /******************* code that runs *******************/
-    
+
     self.fetchAllListings();
-    
+
     //set up the map centered at liberty university
-	initMap({lat:37.353464, lng:-79.177372}, 7);
+    initMap({
+        lat: 37.353464,
+        lng: -79.177372
+    }, 7);
     //add markers to map
     //addMarker(self.detailedListing.latlng);
     self.placeMarkers();
 
-    
-}]);
 
+}]);
--- src/main/webapp/static/js/service/ListingService.js
+++ src/main/webapp/static/js/service/ListingService.js
@@ -35,43 +35,43 @@
 
 //This is only used for local testing. Must be commented out when deploying to openshift.
 
-App.factory('ListingService', ['$http', '$q', function($http, $q){
-	
-	return {
-		
-		fetchAllListings: function() {
-			return $http.get('http://localhost:8080/finderapp/listings/')
-					.then(
-							function(response) {
-//								console.log(response.data);
-								return response.data;
-							},
-							function(errResponse) {
-								console.error('Error while fetching listings');
-								return $q.reject(errResponse);
-							}
-					);
-		},
-		
-		fetchSingleListing: function(id) {
-			return $http.get('http://localhost:8080/finderapp/listings/'+id)
-					.then(
-						function(response) {
-							console.log(response.data);
-							return response.data;
-						},
-						function(errResponse) {
-							return $q.reject(errResponse);
-						}
-					);
-		},
-		
-		createNewListing: function(data) {
-			return $http.post('http://localhost:8080/finderapp/listings/new', data).success(function(data, status) {
-				console.log(data);
-			})
-		}
-	};
+App.factory('ListingService', ['$http', '$q', function ($http, $q) {
+
+    return {
+
+        fetchAllListings: function () {
+            return $http.get('http://localhost:8080/finderapp/listings/')
+                .then(
+                    function (response) {
+                        //console.log(response.data);
+                        return response.data;
+                    },
+                    function (errResponse) {
+                        console.error('Error while fetching listings');
+                        return $q.reject(errResponse);
+                    }
+                );
+        },
+
+        fetchSingleListing: function (id) {
+            return $http.get('http://localhost:8080/finderapp/listings/' + id)
+                .then(
+                    function (response) {
+                        console.log(response.data);
+                        return response.data;
+                    },
+                    function (errResponse) {
+                        return $q.reject(errResponse);
+                    }
+                );
+        },
+
+        createNewListing: function (data) {
+            return $http.post('http://localhost:8080/finderapp/listings/new', data).success(function (data, status) {
+                console.log(data);
+            })
+        }
+    };
 }]);
 
 //var services = angular.module('myApp.ListingService', ['ngResource']);
