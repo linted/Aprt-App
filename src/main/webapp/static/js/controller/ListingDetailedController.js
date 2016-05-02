0411b57c0ef15f2d59490cdffa7b95e15a3d462e
linted <linted90@hotmail.com>
2016-05-02 16:48:55 -0400

    added documentation to detailed listing controller

--- src/main/webapp/static/js/controller/ListingDetailedController.js
+++ src/main/webapp/static/js/controller/ListingDetailedController.js
@@ -2,23 +2,24 @@
 
 App.controller('ListingDetailedController', ['$scope', 'ListingService', '$routeParams', '$cookies', function ($scope, ListingService, $routeParams, $cookies) {
     //variable declarations
-    var self = this;
-    var listing = {};
-    var id;
-
-    id = $routeParams.keyId;
-    $scope.allowEdit = false;
+    var self = this;                                        //
+    var listing = {};                                       //declare the listing variable we are going to use
+    var id = $routeParams.keyId;                            //the ID of the listing we are looking at  
+ 
+    $scope.allowEdit = false;                               //assume that the user is not the owner
 
     /* Function declarations */
-    self.fetchSingleListing = function (id) {
+    self.fetchSingleListing = function (id) {               //function to recieve the listing from the database
         ListingService.fetchSingleListing(id)
             .then(
                 function (d) {
-                    console.log("logging listing");
-                    console.log(d);
+                    // if we got the d, assign it to listing
                     self.listing = d;
+                    //make the map
                     self.map = self.resultsMapInit();
+                    //make sure the map is sized correctly
                     google.maps.event.trigger(self.map, "resize");
+                    //check if the user is allowed to edit this page
                     $scope.allowEdit = ($cookies.get('user') == self.listing.orgId);
                 },
                 function (errResponse) {
@@ -27,18 +28,20 @@ App.controller('ListingDetailedController', ['$scope', 'ListingService', '$route
             );
     };
 
-    self.resultsMapInit = function () {
-        console.log("listings map");
+    self.resultsMapInit = function () {                     //function to init the map and display the location
         if (self.listing.latitude && self.listing.longitude) {
+            //if we have lat lng then set the map approriatly
             var map = initMap("resultsGoogleMaps", {
                 lat: self.listing.latitude,
                 lng: self.listing.longitude
             }, 13);
+            //make sure the add the marker also
             addMarker({
                 lat: self.listing.latitude,
                 lng: self.listing.longitude
             }, map);
         } else {
+            //if we don't have lat lng, center at liberty, don't add a marker
             var map = initMap("resultsGoogleMaps", {
                 lat: 37.391031,
                 lng: -79.191554
@@ -50,7 +53,7 @@ App.controller('ListingDetailedController', ['$scope', 'ListingService', '$route
 
     
     /* Code that runs */
-    self.fetchSingleListing(id);
+    self.fetchSingleListing(id);                            //kick it all off
         
     
 }]);
